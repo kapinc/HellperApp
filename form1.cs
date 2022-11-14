5875aa69c391a2bda48d282dab8a740699429515
S1mpl3ton <titanikpotonyl@gmail.com>
2022-11-14 18:42:25 +0400

    hsdfhfjs

--- form1.cs
+++ form1.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Text;
 
 public class form1 : MonoBehaviour
 {
+    public form1()
+    {
+        InitializeComponent();
+    }
+
+    private void btnClick_Click()
+    {
+        string f = txtfirst.Text;
+        
+        IbiOutput.Text = f;
+    }
 
 }
