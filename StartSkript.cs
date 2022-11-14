1b8d83a45974cf399c294156ef4b574937ae2f6b
kap <maxhawkyt@gmail.com>
2022-11-14 15:36:50 +0400

    commit

---
+++ StartSkript.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.RemoteConfig;
+using UnityEngine.UI;
+
+public class StartSkript : MonoBehaviour
+{
+    public struct userAttributes { }
+    public struct appAttributes { }
+
+    public string kay1;
+
+    public GameObject MenuPan;
+    public GameObject IncorrectPan;
+    public GameObject ShortPan;
+    public GameObject OsnovaPan;
+    public GameObject ProgramPan;
+
+    public InputField InputFieldPromocod;
+
+    private void Start()
+    {
+        ConfigManager.FetchConfigs(new userAttributes(), new appAttributes());
+        ConfigManager.FetchCompleted += Load;
+    }
+
+    private void Load(ConfigResponse response)
+    {
+        kay1 = ConfigManager.appConfig.GetString("kay1");
+    }
+
+    public void InputPromocod()
+    {
+        if(InputFieldPromocod.text.Length == 4)
+        {
+            if(InputFieldPromocod.text == kay1)
+            {
+                MenuPan.SetActive(!MenuPan.activeSelf);
+            }
+            else if (InputFieldPromocod.text != kay1)
+            {
+                IncorrectPan.SetActive(!IncorrectPan.activeSelf);
+            }
+        }
+        else
+        {
+            ShortPan.SetActive(!ShortPan.activeSelf);
+        }
+    }
+
+     public void CloseIncorrectPan()
+    {
+        IncorrectPan.SetActive(!IncorrectPan.activeSelf);
+    }
+
+     public void CloseShortPan()
+    {
+        ShortPan.SetActive(!ShortPan.activeSelf);
+    }
+
+     public void ComplitePan()
+    {
+        MenuPan.SetActive(!MenuPan.activeSelf);
+        OsnovaPan.SetActive(!OsnovaPan.activeSelf);
+        ProgramPan.SetActive(!ProgramPan.activeSelf);
+    }
+}
